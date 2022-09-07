90645e56d764e10f9dd04fa0fdcf333a0dc12658
Benjamin <kodiugos@gmail.com>
2022-09-07 01:45:36 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+# importing the modules
+import PyPDF2
+import pyttsx3
+
+# path of the PDF file
+path = open('Cracking_the_Coding_Interview_6th_Editio.pdf', 'rb')
+
+# creating a PdfFileReader object
+pdfReader = PyPDF2.PdfReader(path)
+
+# the page with which you want to start
+# this will read the page of 25th page.
+from_page = pdfReader.getPage(34)
+
+# extracting the text from the PDF
+text = from_page.extractText()
+
+# reading the text
+speak = pyttsx3.init()
+speak.say(text)
+speak.runAndWait()
+
